825c41a0b4f354b5667960f02fb3475394e5938e
Jack Weinnig <jackweinnig@gmail.com>
2015-03-26 23:22:50 -0500

    MipsFrame

--- src/Mips/MipsFrame.java
+++ src/Mips/MipsFrame.java
@@ -10,6 +10,10 @@ import Frame.AccessList;
 public class MipsFrame extends Frame {
 
   private int count = 0;
+  private static final int wordSize = 4;
+  private int offset = 0; 
+  
+  
   public Frame newFrame(Symbol name, Util.BoolList formals) {
     Label label;
     if (name == null)
@@ -24,10 +28,32 @@ public class MipsFrame extends Frame {
   public MipsFrame() {}
   private MipsFrame(Label n, Util.BoolList f) {
     name = n;
+    // Allocate formals (inherited from Frame.Frame) 
+    formals = allocFormals(0, f);
+  }
+  
+  public int wordSize() { 
+	  return wordSize; 
   }
 
-  private static final int wordSize = 4;
-  public int wordSize() { return wordSize; }
+  public Access allocLocal(boolean escape) { 
+	  if(escape){
+		  offset = offset - wordSize; 
+		  return new InFrame(offset);
+	  }
+	  else
+		  return new InReg(new Temp());
+  }
+  
+  public AccessList allocFormals(int off, Util.BoolList f) {
+	  if(f == null){
+		  return null;
+	  }
+	  else if(f.head){
+		  return new AccessList(new InFrame(off), allocFormals(offset + wordSize, f.tail));
+	  }
+	  else
+		  return new AccessList(new InReg(new Temp()), allocFormals(offset + wordSize, f.tail));
+  }
 
-  public Access allocLocal(boolean escape) { return null; }
 }
